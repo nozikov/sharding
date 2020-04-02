12d9a0c52fa5e53735bde944573a52d116d239dc
Anton Nozikov <nozikov1@gmail.com>
2020-04-02 22:41:52 +0300

    added exception handler

--- src/main/java/ru/ya/sharding/crud/service/PaymentService.java
+++ src/main/java/ru/ya/sharding/crud/service/PaymentService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import ru.ya.sharding.crud.repository.PaymentRepository;
 import ru.ya.sharding.crud.repository.UserRepository;
 import ru.ya.sharding.model.Payment;
+import ru.ya.sharding.model.User;
 
 @Slf4j
 @Service
@@ -18,6 +19,10 @@ public class PaymentService {
 
   public int getStatementOfExpenses(final String name) {
     log.info("Search payments by name: " + name);
+    User user = userRepository.findByName(name);
+    if (user == null) {
+      throw new IllegalArgumentException("User with the same name does not exist: " + name);
+    }
     return paymentRepository.findPaymentsBySender(userRepository.findByName(name));
   }
 
