b204f48a54ed64976af5ecbadb0fa6e744085a06
Anton Nozikov <nozikov1@gmail.com>
2020-04-02 19:18:21 +0300

    fix code style

--- src/main/java/ru/ya/sharding/ShardingApp.java
+++ src/main/java/ru/ya/sharding/ShardingApp.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShardingApp {
 
-
   public static void main(String[] args) {
     SpringApplication.run(ShardingApp.class);
   }
-
 }
