85a70a8ff506dc632cd1d4b1f38e16b96de64750
Anton Nozikov <nozikov1@gmail.com>
2020-04-02 19:14:55 +0300

    Made Payment serializable and added annotation manyToOne for field user

--- src/main/java/ru/ya/sharding/model/Payment.java
+++ src/main/java/ru/ya/sharding/model/Payment.java
@@ -1,21 +1,25 @@
 package ru.ya.sharding.model;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
 @RequiredArgsConstructor
-public class Payment {
+public class Payment implements Serializable {
 
   @Id
   @GeneratedValue
   private long id;
   private int amount;
+  @ManyToOne
   private User sender;
+  @ManyToOne
   private User recipient;
 
   public Payment(int amount, User sender, User recipient) {
